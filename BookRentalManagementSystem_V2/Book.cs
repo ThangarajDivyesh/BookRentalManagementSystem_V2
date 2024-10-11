61a98f45f6a61303d0eff70ffbe43a7af0e4ebba
ThangarajDivyesh <dithakshan7529@gmail.com>
2024-10-11 16:54:33 +0530

    Q10

--- BookRentalManagementSystem_V2/Book.cs
+++ BookRentalManagementSystem_V2/Book.cs
@@ -7,6 +7,12 @@ using System.Threading.Tasks;
 namespace BookRentalManagementSystem_V2
 {
     internal class Book
-    {
+    { 
+        public string BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Rentalprice { get; set; }
+             
+
     }
 }
